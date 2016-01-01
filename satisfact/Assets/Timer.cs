9f9a3b041967df642cfa3ad6bb5b46af65173efd
Matthias DUBRAY <mdubray@e1r5p4.42.fr>
2016-01-01 16:30:46 +0100

    maj

--- satisfact/Assets/Timer.cs
+++ satisfact/Assets/Timer.cs
@@ -22,11 +22,18 @@ public class Timer : MonoBehaviour {
 	private bool		goal = false;
 	private float		init_x;
 
+	private float		init_y_top;
+	private float		init_y_bottom;
+
 	// Use this for initialization
 	void Start () {
 		ntop = panel_top.GetComponent<RectTransform> ().offsetMax;
 		nbottom = panel_bottom.GetComponent<RectTransform> ().offsetMax;
+
 		init_x = plane.transform.position.x;
+
+		init_y_top = ntop.y;
+		init_y_bottom = nbottom.y;
 	}
 	
 	// Update is called once per frame
@@ -34,11 +41,10 @@ public class Timer : MonoBehaviour {
 		if (!goal) {
 			if (elapsed < timer) {
 				elapsed += Time.deltaTime;
-				// manage timers
-				print (ntop.y);
-				ntop.y = (2*width / width) * (elapsed/timer); 
-				nbottom.y = nbottom.y - (elapsed * timer / width);
 
+				// manage timer UI
+				ntop.y = init_y_top - width * elapsed / timer;
+				nbottom.y = init_y_bottom - width * elapsed / timer;
 				panel_top.GetComponent<RectTransform> ().offsetMax = ntop;
 				panel_bottom.GetComponent<RectTransform> ().offsetMax = nbottom;
 
@@ -54,13 +60,12 @@ public class Timer : MonoBehaviour {
 					venti_right.GetComponent<Image>().sprite = venti_right_down;
 				}
 			} else {
-				// fin du game
 				plane.GetComponent<Image>().sprite = plane_broken;
 				venti_left.GetComponent<ParticleSystem>().Stop();
 				print ("Game Over");
 				// Load next level
 			}
-		} else {;
+		} else {
 			venti_left.GetComponent<ParticleSystem>().Stop();
 			print ("Success");
 		}
