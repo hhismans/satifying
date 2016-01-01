408a6a8a786eb37f657200b48fd355ef0c929343
Matthias DUBRAY <mdubray@e1r5p4.42.fr>
2016-01-01 15:45:06 +0100

    maj

--- satisfact/Assets/Timer.cs
+++ satisfact/Assets/Timer.cs
@@ -4,18 +4,20 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour {
 
-	public float		timer = 5f;
+	private float		timer = 5f;
 	private float		elapsed = 0f;
 	public GameObject	panel_top;
 	public GameObject	panel_bottom;
 	private Vector2 	ntop;
 	private Vector2 	nbottom;
-	private int			width = 58;
+	private int			width = 60;
 
 	public GameObject	final_plane;
 	public GameObject	plane;
 	public GameObject	venti_right;
+	public GameObject	venti_left;
 	public Sprite		venti_right_down;
+	public Sprite		plane_broken;
 
 	private bool		goal = false;
 	private float		init_x;
@@ -29,30 +31,37 @@ public class Timer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (elapsed < timer && !goal) {
-			elapsed += Time.deltaTime;
-			// manage timers
-			ntop.y = ntop.y - (elapsed * timer / width);
-			nbottom.y = nbottom.y - (elapsed * timer / width);
+		if (!goal) {
+			if (elapsed < timer) {
+				elapsed += Time.deltaTime;
+				// manage timers
+				print (ntop.y);
+				ntop.y = (2*width / width) * (elapsed/timer); 
+				nbottom.y = nbottom.y - (elapsed * timer / width);
 
-			panel_top.GetComponent<RectTransform> ().offsetMax = ntop;
-			panel_bottom.GetComponent<RectTransform> ().offsetMax = nbottom;
+				panel_top.GetComponent<RectTransform> ().offsetMax = ntop;
+				panel_bottom.GetComponent<RectTransform> ().offsetMax = nbottom;
 
-			// manage plane
-			float x_pos = init_x + (final_plane.transform.position.x - init_x) * elapsed / timer;
-			plane.GetComponent<RectTransform>().transform.position = new Vector2(x_pos, plane.transform.position.y);
+				// manage plane
+				float x_pos = init_x + (final_plane.transform.position.x - init_x) * elapsed / timer;
+				plane.GetComponent<RectTransform>().transform.position = new Vector2(x_pos, plane.transform.position.y);
 
-			// push button ?
-			RaycastHit2D hit = Physics2D.Raycast(Input.mousePosition, Vector2.zero);
-			if(hit.collider != null)
-			{
-				goal = true;
-				venti_right.GetComponent<Image>().sprite = venti_right_down;
+				// push button ?
+				RaycastHit2D hit = Physics2D.Raycast(Input.mousePosition, Vector2.zero);
+				if(hit.collider != null)
+				{
+					goal = true;
+					venti_right.GetComponent<Image>().sprite = venti_right_down;
+				}
+			} else {
+				// fin du game
+				plane.GetComponent<Image>().sprite = plane_broken;
+				venti_left.GetComponent<ParticleSystem>().Stop();
+				print ("Game Over");
+				// Load next level
 			}
 		} else {
-			// fin du game
-			print ("Game Over");
-			// Load next level
+			print ("Success");
 		}
 	}
 }
